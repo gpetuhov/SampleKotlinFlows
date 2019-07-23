b99c07f19b2a4c55a316822e9230f7fb7ca34add
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-07-23 17:27:58 +0300

    Handle button clicks

--- app/src/main/java/com/gpetuhov/android/samplekotlinflows/MainActivity.kt
+++ app/src/main/java/com/gpetuhov/android/samplekotlinflows/MainActivity.kt
@@ -2,11 +2,25 @@ package com.gpetuhov.android.samplekotlinflows
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        start_button.setOnClickListener { startCount() }
+        reset_button.setOnClickListener { resetCount() }
+    }
+
+    // === Private methods ===
+
+    private fun startCount() {
+        // TODO
+    }
+
+    private fun resetCount() {
+        counter.text = "0"
     }
 }
