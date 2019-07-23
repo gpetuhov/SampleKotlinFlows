6f8b1a43846ccbd590daf32e4de5f0f9a922db52
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-07-23 17:36:53 +0300

    Add flow

--- app/src/main/java/com/gpetuhov/android/samplekotlinflows/MainActivity.kt
+++ app/src/main/java/com/gpetuhov/android/samplekotlinflows/MainActivity.kt
@@ -3,7 +3,11 @@ package com.gpetuhov.android.samplekotlinflows
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
 
+@FlowPreview    // currently Kotlin Flows are experimental, so we need this annotation
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,7 +21,19 @@ class MainActivity : AppCompatActivity() {
     // === Private methods ===
 
     private fun startCount() {
-        // TODO
+        // TODO: change this to activity scope
+        GlobalScope.launch(Dispatchers.Main) {
+            val intFlow = flow {
+                (0..10).forEach {
+                    delay(100)
+                    emit(it)
+                }
+            }
+
+            intFlow.collect {
+                counter.text = it.toString()
+            }
+        }
     }
 
     private fun resetCount() {
